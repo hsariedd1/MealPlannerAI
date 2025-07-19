bc06f4c50b1550ffdddaae691f04b5e1baff8002
hsariedd <hsariedd@gmail.com>
2025-07-19 17:58:21 -0500

    Update main.py

--- app/main.py
+++ app/main.py
@@ -9,6 +9,7 @@ from dotenv import load_dotenv
 load_dotenv()
 client = OpenAI(
     api_key=os.getenv("OPENAI_API_KEY"),
+    organization="personal",
     project="proj_GtvTRNh4PBFyjS4vQLDeGIQW" 
 )
 app = FastAPI()
